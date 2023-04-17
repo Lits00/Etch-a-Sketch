efb24c8a38a09d2f275d28e78aeed63a7b4acfc0
Angelito Dionisio <litsdionisio@gmail.com>
2023-04-17 22:19:14 +0400

    Modified hover effect and created random color function

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const defaultGrid = 16;
 const gridContainer = document.querySelector('.grid-container');
 const rangeInput = document.getElementById('rangeInput');
+const randomColorBtn = document.querySelector('.randomColorBtn');
 const clearBtn = document.querySelector('.clearBtn');
 
 // creates the grid
@@ -24,17 +25,34 @@ function reset(){
     boxes.forEach( box => box.remove());
 }
 
-// changes the color of the div on hover
-function hoverEffect(event) {
-    event.target.style.backgroundColor = "black";
-}
-
 // creates the number of divs depending on the input range
 rangeInput.addEventListener('input', function() {
     const gridSize = `${this.value}x${this.value}`
     document.querySelector('.displayValue').value = `Grid Size: ${gridSize}`;
     createDiv(this.value);
 })
+
+// changes the color of the div on hover
+function hoverEffect(event) {
+    if (randomColorBtn.classList.contains('active')) {
+        const red = Math.floor(Math.random() * 256);
+        const green = Math.floor(Math.random() * 256);
+        const blue = Math.floor(Math.random() * 256);
+        event.target.style.backgroundColor = `rgb(${red},${green},${blue})`;
+    } else {
+        event.target.style.backgroundColor = "black";
+    }
+}
+
+// toggles to black or random color hover effect
+randomColorBtn.addEventListener('click', function() {
+    const isActive = randomColorBtn.classList.toggle('active');
+    if (isActive) {
+        randomColorBtn.textContent = 'Black Color';
+    } else {
+        randomColorBtn.textContent = 'Random Color';
+    }
+});
 
 // clears the grid
 clearBtn.addEventListener('click', function() {
